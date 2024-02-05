d9f0ace7a345a4acca23e930f8f69de991284409
fyng <feiyangthegoat@gmail.com>
2024-02-05 18:53:30 -0500

    update test

--- tests/test_gmm.py
+++ tests/test_gmm.py
@@ -5,14 +5,14 @@ from gmm_seg_classifier.gmm import GaussianMixtureModel as GMM
 class TestGMM:
     @pytest.fixture
     def data_fixture(self):
-        means = [[0, 1, 2], [3, 4, 5], [-2, -1, 0]]  # Mean of each Gaussian (3D)
+        means = np.array([[0, 1, 2], [3, 4, 5], [-2, -1, 0]]) # Mean of each Gaussian (3D)
         covariances = [  # Covariance matrices for each Gaussian (3D)
             [[1, 0.1, 0.2], [0.1, 1, 0.1], [0.2, 0.1, 1]],
             [[1, 0.2, 0.1], [0.2, 1, 0.2], [0.1, 0.2, 1]],
             [[1, 0.3, 0.1], [0.3, 1, 0.3], [0.1, 0.3, 1]]
         ]
         mixture_coeff = [0.3, 0.4, 0.3]  # Mixing coefficients (should sum to 1)
-        n_samples = 1000
+        n_samples = 10000
         n_components = len(mixture_coeff)
 
         weights = np.array(mixture_coeff) / np.sum(mixture_coeff)
@@ -27,10 +27,20 @@ class TestGMM:
         model = GMM(n_components=3)
         model.fit(data_fixture)
 
-        print(model.means)
-        print(model.covariance)
-        print(model.mixture_coeff)
+        means = np.array([[0, 1, 2], [3, 4, 5], [-2, -1, 0]])
+        covariances = np.array([
+            [[1, 0.1, 0.2], [0.1, 1, 0.1], [0.2, 0.1, 1]],
+            [[1, 0.2, 0.1], [0.2, 1, 0.2], [0.1, 0.2, 1]],
+            [[1, 0.3, 0.1], [0.3, 1, 0.3], [0.1, 0.3, 1]]
+        ])
+        mixture_coeff = np.array([0.3, 0.4, 0.3])
 
-        assert False
+
+        print(model.means)
+        print(model.mixing_coeff)
+        
+        assert np.allclose(model.means, means, rtol = 0.01, atol = 0.01)
+        assert np.allclose(model.mixing_coeff, mixture_coeff, rtol = 0.01, atol = 0.01)
+        assert np.allclose(model.covariance, covariances, rtol = 1e-3, atol = 1e-3)
 
         
