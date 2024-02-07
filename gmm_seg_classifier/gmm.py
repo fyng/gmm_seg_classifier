5a3556669db69528b2e96729c175fc8f828eeecc
fyng <feiyangthegoat@gmail.com>
2024-02-07 01:12:25 -0500

    update gmm

--- gmm_seg_classifier/gmm.py
+++ gmm_seg_classifier/gmm.py
@@ -1,14 +1,19 @@
 import numpy as np
+from scipy.stats import multivariate_normal
+
+# refer to https://github.com/mr-easy/GMM-EM-Python/blob/master/GMM.py#L58
+
+# CLASS NOTES: restrict mixing component to 1/k. No learning on it
 
 class GaussianMixtureModel:
-    def __init__(self, n_components, n_iter=1000, tol=1e-6, verbose=True):
+    def __init__(self, n_components, n_iter=1000, tol=1e-3, verbose=True):
         self.n_components = n_components 
         self.n_iter = n_iter
         self.tol = tol
         self.verbose = verbose
-        self.means = None # vector. len = self.n_components
-        self.covariance = None # vector. len = self.n_components
-        self.mixing_coeff = None # vector. len = self.n_components
+        self.means = None # [n_components, n_features]
+        self.covariance = None # [n_components, n_features, n_features]
+        self.mixing_coeff = None # [n_components]
         
     def fit(self, X):
         if not isinstance(X,np.ndarray):
@@ -19,7 +24,7 @@ class GaussianMixtureModel:
 
         # Initialize mean as some random datapoints; covarance as identity; equal mixing coefficient
         self.means = X[np.random.choice(n_samples, self.n_components, replace=False)] # k x n_features
-        self.covariance = np.array([np.eye(n_features) for _ in range(self.n_components)])
+        self.covariance = np.array([np.eye(n_features) + np.random.randint((n_features, n_features)) for _ in range(self.n_components)])
         self.mixing_coeff = np.ones(self.n_components) / self.n_components
 
         log_likelihood = 0
@@ -34,46 +39,82 @@ class GaussianMixtureModel:
             log_likelihood = new_log_likelihood
             print(f'Iteration {i}: log-likelihood = {log_likelihood}')
 
+            # #FIXME: run one iteration
+            # break
+
+    def predict(self, X):
+        pass
+
     def _e_step(self, X):
         n_samples, _ = X.shape
         attribution = np.zeros((n_samples, self.n_components))
         for k in range (self.n_components):
             attribution[:,k] = self.mixing_coeff[k] * self._multivariate_normal(X, self.means[k], self.covariance[k])
-        attribution /= attribution.sum(axis=1)[:,None]
+        attribution /= attribution.sum(axis=1, keepdims=True)
 
         return attribution
 
     def _m_step(self, X, attribution): 
         n_samples, _ = X.shape
-        norm = attribution.sum(axis=0)
-        for k in range(self.n_components):
-            norm_k = norm[k]
-            z_k = attribution[:,k]
-            self.means[k] = (np.transpose(X) @ z_k) / norm_k
+        norm = attribution.sum(axis=0) # (n_components, )
 
-            delta = X - self.means[k]
-            self.covariance[k] = (np.transpose(delta * z_k[:,np.newaxis]) @ delta) / norm_k
-        
-        # self.mixing_coeff = norm / norm.sum()
+        # update mixing coefficients
+        self.mixing_coeff = norm / norm.sum()
+
+        # update means
+        self.mean = attribution.T.dot(X) / norm[:, np.newaxis] # (n_components, n_features)
+        # self.mean = np.matmul(attribution.T, X) / norm[:, np.newaxis] # (n_components, n_features)
+
+        # update covariance
+        for k in range(self.n_components):
+            x_centered = X - self.means[k]
+
+            z_k = attribution[:,k] # (n_samples, )
+            weighted_x = z_k[:,np.newaxis] * x_centered
+            self.covariance[k] = weighted_x.T.dot(x_centered) / norm[k]
+
+            # x_centered = np.expand_dims(X, axis=1) - self.mean[k]
+            # s = np.matmul(x_centered.transpose([0, 2, 1]), x_centered)
+            # self.covariance[k] = np.matmul(s.transpose(1, 2, 0), attribution[:, k] )
+            # self.covariance[k] /= norm[k]
+
 
     def _multivariate_normal(self, X, means, covariance):
         # calculate pdf of multivariate normal
+        # _, n_features = X.shape
+        # z = (2*np.pi) ** (n_features/2) * (np.linalg.det(covariance) ** 0.5)
 
-        # means:  n_sample vector
-        # covariance: n_sample x n_sample
-        _, n_features = X.shape
-        z = (2*np.pi) ** (n_features/2) * np.linalg.det(covariance)
+        # x_centered = X - means 
+        # inv_cov = np.linalg.inv(covariance)
+        # exp_term = np.diag(-0.5 * (x_centered.dot(inv_cov).dot(x_centered.T)))
 
-        delta = X - means 
-        inv_cov = np.linalg.inv(covariance)
-        exp_term = -0.5 * np.sum((delta.dot(inv_cov)*delta), axis=1)
-        
-        return np.exp(exp_term) / z
+        # # print(np.allclose(exp_term, exp_term.T))
+
+        # p_x = np.exp(exp_term) / z
+
+        # # https://gregorygundersen.com/blog/2019/10/30/scipy-multivariate/
+        # # `eigh` assumes the matrix is Hermitian.
+        # vals, vecs = np.linalg.eigh(covariance)
+        # logdet     = np.sum(np.log(vals))
+        # valsinv    = np.array([1./v for v in vals])
+        # # `vecs` is R times D while `vals` is a R-vector where R is the matrix 
+        # # rank. The asterisk performs element-wise multiplication.
+        # U          = vecs * np.sqrt(valsinv)
+        # rank       = len(vals)
+        # dev        = X - means
+        # # "maha" for "Mahalanobis distance".
+        # maha       = np.square(np.dot(dev, U)).sum()
+        # log2pi     = np.log(2 * np.pi)
+        # exp_term = -0.5 * (rank * log2pi + maha + logdet)
+        # p_x = np.exp(exp_term)
+
+        return multivariate_normal.pdf(X, means, covariance)
+    
     
     def _get_log_likelihood(self, X):
-        ll = 0
+        likelihood = 0
         for k in range(self.n_components):
-            ll += np.log(np.sum(self._multivariate_normal(X, self.means[k], self.covariance[k])))
+            likelihood += self.mixing_coeff[k] * self._multivariate_normal(X, self.means[k], self.covariance[k])
 
-        return ll
+        return np.log(np.sum(likelihood) / self.n_components)
     
